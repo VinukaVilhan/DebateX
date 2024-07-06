311f3fe5bde56d42c85d57dbb14812b3a421bfc4
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-07-06 23:36:15 +0530

    chore: Add Stream SDK dependencies and token provider for StreamVideoClient

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -1,25 +1,51 @@
-import {
-  StreamCall,
-  StreamVideo,
-  StreamVideoClient,
-  User,
-} from "@stream-io/video-react-sdk";
+import { tokenProvider } from "@/actions/stream.actions";
+import { auth } from "@/lib/firebase/config";
+import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { ReactNode, useEffect, useState } from "react";
 
-const apiKey = "your-api-key";
-const userId = "user-id";
-const token = "authentication-token";
-const user: User = { id: userId };
+const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-const client = new StreamVideoClient({ apiKey, user, token });
-const call = client.call("default", "my-first-call");
-call.join({ create: true });
-
-export const MyApp = () => {
-  return (
-    <StreamVideo client={client}>
-      <StreamCall call={call}>
-        
-      </StreamCall>
-    </StreamVideo>
+export const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient | null>(
+    null
   );
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+    if (!apiKey) {
+      throw new Error("Stream API key is required");
+    }
+
+    const client = new StreamVideoClient({
+      apiKey,
+      user: {
+        id: user.uid,
+        name: user.displayName || user.uid,
+        image: user.photoURL || "",
+      },
+      tokenProvider,
+    });
+
+    setVideoClient(client);
+  }, [user]);
+
+  return videoClient ? (
+    <StreamVideo client={videoClient}>{children}</StreamVideo>
+  ) : null;
 };
+
+export default StreamVideoProvider;
