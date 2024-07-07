903fa5c660ae064bebf20c00b98d6c716841a5ca
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-07-07 19:36:22 +0530

    chore: Refactor code to use client-side rendering for images

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,23 +1,28 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+
+import Image from "next/image";
+import React from "react";
 
 type MeetingTypeListProps = {
-  img: string
-  title: string
-}
+  img: string;
+  title: string;
+  handleClick?: () => void;
+};
 
-const MeetingTypeList = ({img, title}: MeetingTypeListProps) => {
+const MeetingTypeList = ({ img, title, handleClick }: MeetingTypeListProps) => {
   return (
-    <div className='flex flex-col gap-1 my-auto'>
+    <div className="flex flex-col gap-1 my-auto">
       <Image
-      src={img}
-      width={40}
-      height={40}
-      alt={title}
+        src={img}
+        width={40}
+        height={40}
+        alt={title}
+        onClick={handleClick}
+        className="cursor-pointer"
       />
-      <p className='text-sm text-black'>{title}</p>
+      <p className="text-sm text-black">{title}</p>
     </div>
-  )
-}
+  );
+};
 
-export default MeetingTypeList
+export default MeetingTypeList;
