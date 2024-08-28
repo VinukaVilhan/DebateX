db8c5b8111b6b897c85a7b7b4ef4b14e46a70f38
Jalina Hirushan <gunathungagajh.22@uom.lk>
2024-08-29 01:14:14 +0530

    refactor: Import Alert component in the correct order

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,9 +7,9 @@ import { useParams } from "next/navigation";
 import { Loader } from "lucide-react";
 
 import { useGetCallById } from "@/hooks/useGetCallById";
+import Alert from "@/components/Alert";
 import MeetingSetup from "@/components/MeetingSetup";
 import MeetingRoom from "@/components/MeetingRoom";
-import Alert from "@/components/Alert";
 
 const MeetingPage = () => {
   const { id } = useParams();
